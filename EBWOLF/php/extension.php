514d8e5f02b6418823e9573a412c857ded293556
johan211315 <johanaricapa@gmail.com>
2025-03-25 22:38:27 +0100

    prueba rama 2

---
+++ EBWOLF/php/extension.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ca">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Arctic wolves login</title>
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="../css/panda.css" />
+</head>
+
+<body>
+    <div class="container">
+        <form action="validar.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" id="username" placeholder="Enter username" /><br />
+            <label for="password">Password</label>
+            <input type="password" id="password" placeholder="Password" /><br />
+            <button>Login</button>
+        </form>
+        <div class="ear-l"></div>
+        <div class="ear-r"></div>
+        <div class="panda-face">
+            <div class="blush-l"></div>
+            <div class="blush-r"></div>
+            <div class="eye-l">
+                <div class="eyeball-l"></div>
+            </div>
+            <div class="eye-r">
+                <div class="eyeball-r"></div>
+            </div>
+            <div class="nose"></div>
+            <div class="mouth"></div>
+        </div>
+        <div class="hand-l"></div>
+        <div class="hand-r"></div>
+        <div class="paw-l"></div>
+        <div class="paw-r"></div>
+    </div>
+    <!-- Script -->
+    <script src="../js/panda.js"></script>
+</body>
+
+</html>
